fbfbb523dbf05257f8b2377d50c1f4fb08cca659
miss-madina <250135@newuu.uz>
2026-02-10 12:25:33 +0500

    lab3 commit

--- lab3.cpp
+++ lab3.cpp
@@ -2,6 +2,48 @@
 // Created by Madinabonu Mirzaxmatova on 10/02/26.
 //
 #include <iostream>
+using namespace std;
+int main() {
+    // 1. Integer variable
+    int intNumber = 30;   // number of students in the lab
+
+    // 2. Float variable
+    float floatNumber = 3.14f;   // value of pi
+
+    // 3. Double variable
+    double doubleNumber = 45.1234;
+
+    // 4. Boolean variable
+    bool boolean = true;
+
+    // 5. Character variable
+    char charName = 'A';
+
+    // 6. Print values and sizes
+    cout << "Number of Students =" << intNumber
+         << ", Size = " << sizeof(intNumber) << " bytes" << endl;
+
+    cout << "PI =" << floatNumber
+         << ", Size = " << sizeof(floatNumber) << " bytes" << endl;
+
+    cout << "double variable name= " << doubleNumber
+         << ", Size = " << sizeof(doubleNumber) << " bytes" << endl;
+
+    cout << " bool type variable name = " << boolean
+         << ", Size = " << sizeof(boolean) << " bytes" << endl;
+
+    cout << "charName = " << charName
+         << ", Size = " << sizeof(charName) << " bytes" << endl;
+
+     cout << "\"I'am standing\" on the edge of some crazy cliff.\n What I have to do,"
+             "I have to catch everybody\\\n if they start to go over cliff-I maean\\n"
+             "\\if they're running and they don't look\n where they're going I have to come out \n"
+             "from somewhere and \t catch them.";
+
+    return 0;
+
+}
+
 
 
 
