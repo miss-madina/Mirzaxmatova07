492be4cd3bf714a99a28d98daedcb72ff1fcc957
miss-madina <250135@newuu.uz>
2026-02-17 12:51:09 +0500

    lab4 commit

--- lab3.cpp
+++ lab3.cpp
@@ -1,118 +1,118 @@
 //
 // Created by Madinabonu Mirzaxmatova on 10/02/26.
+// //
+// #include <iostream>
+// #include <cmath>
 //
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-int main() {
-    // 1. Integer variable
-    // int intNumber = 30;   // number of students in the lab
-    //
-    // // 2. Float variable
-    // float floatNumber = 3.14f;   // value of pi
-    //
-    // // 3. Double variable
-    // double doubleNumber = 45.1234;
-    //
-    // // 4. Boolean variable
-    // bool boolean = true;
-    //
-    // // 5. Character variable
-    // char charName = 'A';
-    //
-    // // 6. Print values and sizes
-    //  cout << "\t \t PROBLEM1"<< endl;
-    // cout << "Number of Students =" << intNumber
-    //      << ", Size = " << sizeof(intNumber) << " bytes" << endl;
-    //
-    // cout << "PI =" << floatNumber
-    //      << ", Size = " << sizeof(floatNumber) << " bytes" << endl;
-    //
-    // cout << "double variable name= " << doubleNumber
-    //      << ", Size = " << sizeof(doubleNumber) << " bytes" << endl;
-    //
-    // cout << " bool type variable name = " << boolean
-    //      << ", Size = " << sizeof(boolean) << " bytes" << endl;
-    //
-    // cout << "charName = " << charName
-    //      << ", Size = " << sizeof(charName) << " bytes" << endl;
-    //  cout << "\t \t PROBLEM2"<< endl;
-    //
-    //  cout << "\"I'am standing\" on the edge of some crazy cliff.\n What I have to do,"
-    //          "I have to catch everybody\\\n if they start to go over cliff-I mean\n"
-    //          "\\if they're running and they don't look\n where they're going I have to come out \n"
-    //          "from somewhere and \t \t catch them."<<endl;
-    //  cout << "\t \t PROBLEM3"<<endl;
-    //  int a=189;
-    //  char b='B';
-    //  float number=a/3;
-    //  double value=b;
-    //  float y=(a+b)/5;
-    //  double character=(a/10)-1;
-    //  cout << number<<endl;
-    //  cout<< value<<endl;
-    //  cout<< y<<endl;
-    //  cout << char(character+(double)b) << endl;
-    //  // cout << "\t \t PROBLEM4" << endl;
-    //  // cin >> a;
-    //  // cin >> b;
-    //  // cout << "Sum =" << a+b << endl;
-    //
-    //  cout << "\t \t PROBLEM5" << endl;
-    //  int N;
-    //  cin >> N;
-
-    //  cout << "\t \t PROBLEM6"<< endl;
-    // int x;
-    // cin >> x;
-    // int y;
-    // cin >> y;
-    // cout << "t = " << float ((3*(x^2+3))/((y/4)+3));
-    // cout << "\t \t PROBLEM7" << endl;
-    // int diameter;
-    // int PI=3.14159;
-    // cin >> diameter;
-    // cout << 4*PI*(diameter/2)^2;
-    //
-    // cout << "PROBLEM11" << endl;
-    // float a;
-    // cout << "a = ";
-    // cin >> a;
-    // int b = a;
-    // cout << "b = ";
-    // cout << b << endl;
-    // float c = a-b;
-    //  cout << "Input is = " << a << endl << "result is = " << c << endl;
-    // cout << "\t \t PROBLEM12" << endl;
-    // int Student_number;
-    // cin >> Student_number;
-    // int c=Student_number/11;
-    // float b=Student_number - c*11;
-    // cout << "In first group:" << c <<endl << "In last group:" << b << endl;
-    // cout << "\t \t PROBLEM21" << endl;
-    // int n;
-    // cin >>n;
-    // int s;
-    // cin >> s;
-    // float PI = 3.14;
-    // cout << "Area = " << (n*(s*s))/(4*tan(PI/n)) << endl;
-    // cout << "\t \t PROBLEM21" << endl;
-    // int h;
-    // cin >> h;
-    // int R;
-    // cin >> R;
-    // int r;
-    // cin >>r;
-    // float PI=3.14;
-    // cout << "V = "<< (PI*h*(R*R+R*r+r*r))/3;
-    //
-    //
-    // return 0;
-
-}
-
-
+// using namespace std;
+// int main() {
+//     // 1. Integer variable
+//     // int intNumber = 30;   // number of students in the lab
+//     //
+//     // // 2. Float variable
+//     // float floatNumber = 3.14f;   // value of pi
+//     //
+//     // // 3. Double variable
+//     // double doubleNumber = 45.1234;
+//     //
+//     // // 4. Boolean variable
+//     // bool boolean = true;
+//     //
+//     // // 5. Character variable
+//     // char charName = 'A';
+//     //
+//     // // 6. Print values and sizes
+//     //  cout << "\t \t PROBLEM1"<< endl;
+//     // cout << "Number of Students =" << intNumber
+//     //      << ", Size = " << sizeof(intNumber) << " bytes" << endl;
+//     //
+//     // cout << "PI =" << floatNumber
+//     //      << ", Size = " << sizeof(floatNumber) << " bytes" << endl;
+//     //
+//     // cout << "double variable name= " << doubleNumber
+//     //      << ", Size = " << sizeof(doubleNumber) << " bytes" << endl;
+//     //
+//     // cout << " bool type variable name = " << boolean
+//     //      << ", Size = " << sizeof(boolean) << " bytes" << endl;
+//     //
+//     // cout << "charName = " << charName
+//     //      << ", Size = " << sizeof(charName) << " bytes" << endl;
+//     //  cout << "\t \t PROBLEM2"<< endl;
+//     //
+//     //  cout << "\"I'am standing\" on the edge of some crazy cliff.\n What I have to do,"
+//     //          "I have to catch everybody\\\n if they start to go over cliff-I mean\n"
+//     //          "\\if they're running and they don't look\n where they're going I have to come out \n"
+//     //          "from somewhere and \t \t catch them."<<endl;
+//     //  cout << "\t \t PROBLEM3"<<endl;
+//     //  int a=189;
+//     //  char b='B';
+//     //  float number=a/3;
+//     //  double value=b;
+//     //  float y=(a+b)/5;
+//     //  double character=(a/10)-1;
+//     //  cout << number<<endl;
+//     //  cout<< value<<endl;
+//     //  cout<< y<<endl;
+//     //  cout << char(character+(double)b) << endl;
+//     //  // cout << "\t \t PROBLEM4" << endl;
+//     //  // cin >> a;
+//     //  // cin >> b;
+//     //  // cout << "Sum =" << a+b << endl;
+//     //
+//     //  cout << "\t \t PROBLEM5" << endl;
+//     //  int N;
+//     //  cin >> N;
+//
+//     //  cout << "\t \t PROBLEM6"<< endl;
+//     // int x;
+//     // cin >> x;
+//     // int y;
+//     // cin >> y;
+//     // cout << "t = " << float ((3*(x^2+3))/((y/4)+3));
+//     // cout << "\t \t PROBLEM7" << endl;
+//     // int diameter;
+//     // int PI=3.14159;
+//     // cin >> diameter;
+//     // cout << 4*PI*(diameter/2)^2;
+//     //
+//     // cout << "PROBLEM11" << endl;
+//     // float a;
+//     // cout << "a = ";
+//     // cin >> a;
+//     // int b = a;
+//     // cout << "b = ";
+//     // cout << b << endl;
+//     // float c = a-b;
+//     //  cout << "Input is = " << a << endl << "result is = " << c << endl;
+//     // cout << "\t \t PROBLEM12" << endl;
+//     // int Student_number;
+//     // cin >> Student_number;
+//     // int c=Student_number/11;
+//     // float b=Student_number - c*11;
+//     // cout << "In first group:" << c <<endl << "In last group:" << b << endl;
+//     // cout << "\t \t PROBLEM21" << endl;
+//     // int n;
+//     // cin >>n;
+//     // int s;
+//     // cin >> s;
+//     // float PI = 3.14;
+//     // cout << "Area = " << (n*(s*s))/(4*tan(PI/n)) << endl;
+//     cout << "\t \t PROBLEM21" << endl;
+//     int h;
+//     cin >> h;
+//     int R;
+//     cin >> R;
+//     int r;
+//     cin >>r;
+//     float PI=3.14;
+//     cout << "V = "<< (PI*h*(R*R+R*r+r*r))/3;
+//
+//
+//     return 0;
+//
+// }
+//
+//
 
 
 
---
+++ lab4.cpp
@@ -0,0 +1,96 @@
+//
+// Created by Madinabonu Mirzaxmatova on 17/02/26.
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+   /* int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    int c;
+    cin >> c;
+    int m=(a+b+c)/3;
+    cout << "Var=" << float ((a-m)*(a-m)+ (b-m)*(b-m)+(c-m)*(c-m))/3;
+
+    int N;
+    cout << "Enter the number ";
+    cin >> N;
+    if (N > 0)
+        cout << "positive";
+    if (N<0)
+        cout << "negative";
+    else
+        cout << "zero";
+
+    int M;
+    cout << "Enter a number = ";
+    cin >> M;
+    if (M%2 ==0)
+        cout << "EVEN";
+    else cout << "ODD";
+
+
+    int A;
+    cout << "enter 1st number ";
+    cin >> A;
+    int B;
+    cout << "enter 2nd number ";
+    cin >> B;
+    if (A>B)
+        cout << "A is largest";
+    else if (B>A)
+        cout << "B is largest";
+    else
+        cout << "Numbers are equal";
+
+    int a;
+    cout << "a = ";
+    cin >> a;
+    int b;
+    cout << "b = ";
+    cin >> b;
+    int c;
+    cout << "c = ";
+    cin >> c;
+    if (a > b && a > c)
+        cout << "a is largest";
+    else if (b > c && b > a)
+        cout << "b is largest";
+    else if ( c > a && c > a)
+        cout << "c is the largest";
+    else
+        cout << "Numbers are equal";
+
+
+    int N;
+    cout << "enter the number";
+    cin >> N;
+    if (N < 20)
+        cout << "too slow";
+    else if (N >80)
+        cout << "too fast";
+    else
+        cout << "just right";
+
+    float N;
+    cout << "Enter N ";
+    cin >> N;
+    if (N >= 50)
+        cout << "pass";
+    else
+        cout << "fail";
+
+    int  M;
+    cout << "Enter the number ";
+    cin >> M;
+    int N;
+    cout << "Enter the number ";
+    cin >> N;
+    if (M%N == 0)
+        cout << M << " is divisible by " << N;
+    else
+        cout << M << " is not divisible by " << N;
+*/
+}
